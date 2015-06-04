72bec19ce13e656dc9285768adc308c5c66e39ae
rapidshot <rapidshotdaw@gmail.com>
2015-06-04 11:00:40 +0000

    Deleted files manually

--- server/util/mongodb.js
+++
@@ -1,9 +0,0 @@
-var config = require('./config');
-var mongo = require('mongoskin');
-
-var db = mongo.db(config.db.conn, {native_parser:true});
-
-module.exports = {
-	db: db,
-	toObjectID: mongo.helper.toObjectID
-};
